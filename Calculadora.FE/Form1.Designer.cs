ecf6f465e8b1013f03824c0c9dbe6fee99dbcec6
Nahuel M. Alvarez <nahuelalvarez153@hotmail.com>
2024-08-21 10:21:35 -0300

    Agregar archivos de proyecto.

---
+++ Calculadora.FE/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Calculadora.FE
+{
+    partial class Calculadora
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOp1 = new System.Windows.Forms.TextBox();
+            this.txtOp2 = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btSumar = new System.Windows.Forms.Button();
+            this.lblOp1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtOp1
+            // 
+            this.txtOp1.Location = new System.Drawing.Point(12, 82);
+            this.txtOp1.Name = "txtOp1";
+            this.txtOp1.Size = new System.Drawing.Size(100, 22);
+            this.txtOp1.TabIndex = 0;
+            // 
+            // txtOp2
+            // 
+            this.txtOp2.Location = new System.Drawing.Point(199, 82);
+            this.txtOp2.Name = "txtOp2";
+            this.txtOp2.Size = new System.Drawing.Size(100, 22);
+            this.txtOp2.TabIndex = 1;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(335, 82);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(100, 22);
+            this.txtResultado.TabIndex = 2;
+            // 
+            // btSumar
+            // 
+            this.btSumar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSumar.Location = new System.Drawing.Point(118, 63);
+            this.btSumar.Name = "btSumar";
+            this.btSumar.Size = new System.Drawing.Size(75, 31);
+            this.btSumar.TabIndex = 3;
+            this.btSumar.Text = "+";
+            this.btSumar.UseVisualStyleBackColor = true;
+            this.btSumar.Click += new System.EventHandler(this.btSumar_Click);
+            // 
+            // lblOp1
+            // 
+            this.lblOp1.AutoSize = true;
+            this.lblOp1.Location = new System.Drawing.Point(12, 63);
+            this.lblOp1.Name = "lblOp1";
+            this.lblOp1.Size = new System.Drawing.Size(72, 16);
+            this.lblOp1.TabIndex = 4;
+            this.lblOp1.Text = "Operador1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(196, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Operador2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(305, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "=";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(332, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Resultado";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(118, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 28);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "X";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Calculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 232);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblOp1);
+            this.Controls.Add(this.btSumar);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtOp2);
+            this.Controls.Add(this.txtOp1);
+            this.Name = "Calculadora";
+            this.Text = "Calculadora";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOp1;
+        private System.Windows.Forms.TextBox txtOp2;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btSumar;
+        private System.Windows.Forms.Label lblOp1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Calculadora.FE/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora.FE
+{
+    public partial class Calculadora : Form
+    {
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btSumar_Click(object sender, EventArgs e)
+        {
+            int Op1;
+            int Op2;
+            int Resultado;
+
+            Op1 = Convert.ToInt32(txtOp1.Text);
+            Op2 = Convert.ToInt32(txtOp2.Text);
+
+            Resultado = Op1 + Op2;
+
+            
+            txtResultado.Text = Resultado.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Op1;
+            int Op2;
+            int Resultado;
+
+            Op1 = Convert.ToInt32(txtOp1.Text);
+            Op2 = Convert.ToInt32(txtOp2.Text);
+
+            Resultado = Op1 * Op2;
+
+            
+            txtResultado.Text = Resultado.ToString();
+        }
+    }
+}
